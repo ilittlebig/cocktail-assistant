29ef5bb4f797887480027d574c77b994b25c0bab
Elias Sjödin <elias@amsystem.com>
2024-10-29 19:10:59 +0100

    add search bar, button & cocktail card component
    No unit tests or functionality has been added, only focusing on the markup at the moment.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /**
- * This file is the entry point for the application.
+ * Entry point for the application.
  *
  * Author: Elias Sjödin
  * Created: 2024-10-29
@@ -8,20 +8,56 @@
 import { LitElement, html, css } from "lit";
 import { customElement } from "lit/decorators.js";
 
+import "./components/search-bar";
+import "./components/cocktail-card";
+import "./components/button-element";
+
 @customElement("app-root")
 export class AppRoot extends LitElement {
   static styles = css`
-    .app-container {
-      padding: 10px;
+    .app-title {
+      color: var(--text-color);
       text-align: center;
-      background-color: #f3f3f3;
+      margin: 0;
+    }
+    .app-container {
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+      margin: 0 auto;
+      padding: 64px;
+      max-width: 900px;
+    }
+
+    .cocktails-container {
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+    }
+
+    .shopping-list-container {
+    }
+
+    search-bar {
+      max-width: 600px;
+      width: 100%;
+      margin: 0 auto;
+      display: block;
     }
   `;
 
   protected render() {
     return html`
       <div class="app-container">
-        <h1>Cocktail Assistant</h1>
+        <h1 class="app-title">Cocktail Assistant</h1>
+        <search-bar placeholder="Search for a cocktail..."></search-bar>
+        <div>
+          <div class="cocktails-container">
+            <cocktail-card imageSrc="https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg"></cocktail-card>
+          </div>
+          <div class="shopping-list-container">
+          </div>
+        </div>
       </div>
     `;
   }
